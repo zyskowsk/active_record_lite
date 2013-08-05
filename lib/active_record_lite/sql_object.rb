8c2b6c2b4e879b00ba00d2b9c36dfafec760e4e2
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-08-05 10:27:50 -0700

    Implemented find, and all

--- lib/active_record_lite/sql_object.rb
+++ lib/active_record_lite/sql_object.rb
@@ -2,18 +2,30 @@ require_relative './associatable'
 require_relative './db_connection'
 require_relative './mass_object'
 require_relative './searchable'
+require 'active_support/inflector'
 
 class SQLObject < MassObject
   def self.set_table_name(table_name)
+    @table_name = table_name
   end
 
   def self.table_name
+    @table_name.underscore
   end
 
   def self.all
+    DBConnection.execute <<-SQL
+      SELECT * 
+        FROM "#{table_name}"
+    SQL
   end
 
   def self.find(id)
+    DBConnection.execute(<<-SQL, id).first
+      SELECT * 
+      FROM "#{table_name}"
+      WHERE id = ?
+    SQL
   end
 
   def create
