fa29b4e292b76f9a76d37ef71b1dcc4f76b5c72d
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-08-05 11:48:03 -0700

    Done with searchable

--- lib/active_record_lite/searchable.rb
+++ lib/active_record_lite/searchable.rb
@@ -2,5 +2,16 @@ require_relative './db_connection'
 
 module Searchable
   def where(params)
+  	where_line = params.keys.map do |attr|
+  		"#{attr} = ?"
+  	end.join("AND ")
+
+  	values = params.values
+
+  	DBConnection.execute(<<-SQL, *values)
+  		SELECT * 
+    		FROM #{table_name}
+    	 WHERE #{where_line}
+  	SQL
   end
 end
