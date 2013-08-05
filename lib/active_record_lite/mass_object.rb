fafe971de2b5fb9e14dfd69f600d3b4579806d0b
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-08-05 10:06:34 -0700

    Done with initialize

---
+++ lib/active_record_lite/mass_object.rb
@@ -0,0 +1,26 @@
+class MassObject
+  def self.set_attrs(*attributes)
+  	@attributes = attributes
+
+  	attributes.each do |attr|
+  		attr_accessor attr
+  	end
+  end
+
+  def self.attributes
+  	@attributes
+  end
+
+  def self.parse_all(results)
+  end
+
+  def initialize(params = {})
+  	params.each do |attr_name, value|
+  		unless self.class.attributes.include?(attr_name)
+  			raise "mass assignment to unregistered attribute #{attr_name}"
+  		end
+
+  		self.send("#{attr_name}=", value)
+  	end
+  end
+end
